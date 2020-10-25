a4b1f068b05abd95d8e8d7fd40e44a4704926617
Wing Ming Sam <wmsam91@gmail.com>
2020-10-25 23:44:48 +0800

    6

--- confusion/src/app/dishdetail/dishdetail.component.ts
+++ confusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { DishService } from '../services/dish.service';
+
 import { Dish } from '../shared/dish';
 @Component({
   selector: 'app-dishdetail',
@@ -8,9 +13,17 @@ import { Dish } from '../shared/dish';
 export class DishdetailComponent implements OnInit {
   @Input()
   dish: Dish;
-  constructor() { }
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit() {
+      const id = this.route.snapshot.params['id'];
+      this.dish = this.dishservice.getDish(id);
+      // alert(JSON.stringify(this.dish));
+    }
 
+    goBack(): void {
+      this.location.back();
+    }
 }
